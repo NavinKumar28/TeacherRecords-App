f84bbd690134054aee8323fd2302623c1d47c667
Navin <Navin@DELL>
2024-01-11 16:59:57 +0530

    adding all files

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teacher_Records_App
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Teacher Records System");
+
+            while (true)
+            {
+                Console.WriteLine("1. Add Teacher");
+                Console.WriteLine("2. View All Teachers");
+                Console.WriteLine("3. Exit");
+
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        AddTeacher();
+                        break;
+                    case 2:
+                        ViewAllTeachers();
+                        break;
+                    case 3:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Try again.");
+                        break;
+                }
+            }
+        }
+
+        static void AddTeacher()
+        {
+            Teacher teacher = new Teacher();
+
+            Console.Write("Enter ID: ");
+            teacher.ID = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Name: ");
+            teacher.Name = Console.ReadLine();
+
+            Console.Write("Enter Class and Section: ");
+            teacher.ClassSection = Console.ReadLine();
+
+            TeacherDataAccess.SaveTeacher(teacher);
+            Console.WriteLine("Teacher added successfully!");
+        }
+
+        static void ViewAllTeachers()
+        {
+            var teachers = TeacherDataAccess.GetAllTeachers();
+
+            Console.WriteLine("Teacher Records:");
+            foreach (var teacher in teachers)
+            {
+                Console.WriteLine($"ID: {teacher.ID}, Name: {teacher.Name}, Class and Section: {teacher.ClassSection}");
+            }
+        }
+    }
+
+}
---
+++ TeacherDataAccess.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teacher_Records_App
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    public class TeacherDataAccess
+    {
+        private const string FilePath = "teachers.txt";
+
+        public static List<Teacher> GetAllTeachers()
+        {
+            List<Teacher> teachers = new List<Teacher>();
+
+            if (File.Exists(FilePath))
+            {
+                string[] lines = File.ReadAllLines(FilePath);
+
+                foreach (string line in lines)
+                {
+                    string[] data = line.Split(',');
+                    Teacher teacher = new Teacher
+                    {
+                        ID = int.Parse(data[0]),
+                        Name = data[1],
+                        ClassSection = data[2]
+                    };
+                    teachers.Add(teacher);
+                }
+            }
+
+            return teachers;
+        }
+
+        public static void SaveTeacher(Teacher teacher)
+        {
+            using (StreamWriter writer = File.AppendText(FilePath))
+            {
+                writer.WriteLine($"{teacher.ID},{teacher.Name},{teacher.ClassSection}");
+            }
+        }
+    }
+
+}
